ee5ae49c689ff65e8cf0c6691e054fd2b874621c
ssc-bando <satoshi_bando@solver.co.jp>
2020-03-02 13:47:02 +0900

    succeed encrypt/decrypt

--- app/src/main/java/com/example/securedatastorage/MainActivity.java
+++ app/src/main/java/com/example/securedatastorage/MainActivity.java
@@ -1,4 +1,4 @@
-// TODO Base64を辞める
+// TODO Base64はSharedPreference保存を意識した機構らしいので必要
 // TODO SharedPreferenceにデータを保存する
 // TODO SharedPreferenceを消したりするボタンを作る
 
@@ -17,40 +17,56 @@ import android.widget.TextView;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
-import java.security.KeyPair;
+import java.security.InvalidKeyException;
 import java.security.KeyPairGenerator;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.UnrecoverableKeyException;
+import java.security.spec.MGF1ParameterSpec;
 
 import javax.crypto.Cipher;
 import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.OAEPParameterSpec;
+import javax.crypto.spec.PSource;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "KeyStoreProviderSample";
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private TextView encryptTextView;
+    private TextView encryptedResultView;
+    private TextView decryptResultView;
     private TextView logView;
     private KeyStore mKeyStore;
     private String providerName;
     private String encryptAlgorithm;
+    private String aliasName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        encryptTextView = (TextView)findViewById(R.id.encryptText);
+        encryptedResultView = (TextView)findViewById(R.id.encryptedText);
+        decryptResultView = (TextView)findViewById(R.id.decryptedOutputView);
         logView = (TextView)findViewById(R.id.LogText);
 
         encryptAlgorithm = getString(R.string.EncryptAlgorith);
         providerName = getString(R.string.KeyProviderName);
-        prepareKeyStore(getString(R.string.SecureKeyAlias), providerName);
+        aliasName = getString(R.string.SecureKeyAlias);
+        prepareKeyStore(aliasName, providerName);
     }
 
     private void log(String s){
         logView.append(s + "\n");
+        Log.d(TAG, s);
     }
 
     private void prepareKeyStore(String alias, String providerName) {
@@ -84,12 +100,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String encryptString(KeyStore keyStore, String alias, String plainText) {
+        log("start encrypt w/ [" + plainText + "]");
         String encryptedText = null;
         try {
             PublicKey publicKey = keyStore.getCertificate(alias).getPublicKey();
 
+            // work arround:
+            //   need OAEPParameterSpec for Cipher#init
+            //   ref: https://developer.android.com/reference/android/security/keystore/KeyGenParameterSpec.html
+            //   ref: https://stackoverflow.com/questions/36015194/android-keystoreexception-unknown-error
+            //   ref: https://teratail.com/questions/122006
+            OAEPParameterSpec spec = new OAEPParameterSpec(
+                    "SHA-256", "MGF1", MGF1ParameterSpec.SHA1, PSource.PSpecified.DEFAULT);
             Cipher cipher = Cipher.getInstance(encryptAlgorithm);
-            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey, spec);
 
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             CipherOutputStream cipherOutputStream = new CipherOutputStream(
@@ -102,10 +126,13 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             Log.e(TAG, e.toString());
         }
+
+        log("encrypted to [" + encryptedText + "]");
         return encryptedText;
     }
 
-    private String decryptString(KeyStore keyStore, String alias, String encryptedText) {
+    private String decryptString(KeyStore keyStore, String alias, String encryptedText) throws NoSuchPaddingException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, InvalidKeyException, IOException {
+        log("> decryptString [" + encryptedText + "]");
         String plainText = null;
         try {
             PrivateKey privateKey = (PrivateKey) keyStore.getKey(alias, null);
@@ -118,29 +145,44 @@ public class MainActivity extends AppCompatActivity {
 
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             int b;
-            while ((b = cipherInputStream.read()) != -1) {
+            while (true){
+                b = cipherInputStream.read();
+                if( b == -1 ){
+                    break;
+                }
                 outputStream.write(b);
             }
             outputStream.close();
             plainText = outputStream.toString("UTF-8");
         } catch (Exception e) {
             Log.e(TAG, e.toString());
+            throw e;
         }
+        log("< decrypted to [" + plainText +"]");
         return plainText;
     }
 
 
-    public void onStoreButtonClicked(View view) throws NoSuchProviderException, NoSuchAlgorithmException, InvalidAlgorithmParameterException {
-        log("> store");
-        log("< store");
+    public void onEncryptButtonClicked(View view) throws NoSuchProviderException, NoSuchAlgorithmException, InvalidAlgorithmParameterException {
+        log("> encrypt");
+        String input = encryptTextView.getText().toString();
+        String result = encryptString(mKeyStore, aliasName, input);
+        encryptedResultView.setText(result);
+        log("< encrypt");
     }
 
-    public void onLoadButtonClicked(View view) {
-        log("> load");
-        log("< load");
+    public void onDecryptButtonClicked(View view) throws NoSuchPaddingException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, InvalidKeyException, IOException {
+        log("> decrypt");
+        String input = encryptedResultView.getText().toString();
+        String result = decryptString(mKeyStore, aliasName, input);
+        decryptResultView.setText(result);
+        log("< [" + input + "] is decrypted [" + result + "]");
     }
 
     public void onClearButtonClicked(View view) {
         logView.setText(null);
+        encryptTextView.setText(getString(R.string.encrypt_string_default));
+        encryptedResultView.setText(getString(R.string.encrypted_string_default));
+        decryptResultView.setText(null);
     }
 }
